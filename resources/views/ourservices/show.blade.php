78571b6d006ec00392e63bf1de2436e136373567
mital <mital.k@biztechnosys.com>
2024-03-14 21:45:02 +0530

    our services

---
+++ resources/views/ourservices/show.blade.php
@@ -0,0 +1,86 @@
+@include('layouts.headermodule')
+
+<body class="hold-transition sidebar-mini zw_sidebar">
+    <div class="wrapper">
+        @include('layouts.header')
+        @include('layouts.sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="row  mb-3">
+                    <div class="col-sm-6 zw_back">
+                        <a href="{{ route('ourservices.index') }}" class="poppins-medium zw_18 zw_text_333333 zw_a zw_back">
+                            <i class="fas fa-arrow-left right mr-2"></i>Back
+                        </a>
+                    </div>
+                    <div class="col-sm-6 ">
+                        <a class="btn zw_btn float-right m-2" href="{{ route('ourservices.edit',$ourservice->id) }}">Update</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- <a class="btn btn-primary m-2" href="{{ route('ourservices.index') }}"> Back</a>
+                    <a class="btn btn-primary float-right m-2" href="{{ route('ourservices.edit',$ourservice->id) }}">Update</a> -->
+                    <!-- left column -->
+                    <div class="col-md-12">
+                        <h3 class="poppins-semibold zw_34 zw_text_AF2245 mt-4 mb-4">Show Main Services</h3>
+
+                        <!-- general form elements -->
+                        <div class="card card-primary">
+                            <!-- <div class="card-header">
+                                <h3 class="card-title">Show Main Services</h3>
+                            </div> -->
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <div class="card-body">
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-4">
+                                        <label class="zw_text_AF2245 poppins-bold" for="exampleInputLogo">Logo :</label>
+                                        <img src="{{ asset($ourservice->Logo) }}" height="" width="50"/>
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold" for="exampleInputEnname">En name :</label>
+                                        {{ $ourservice->Enname }}
+                                    </div>
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold" for="exampleInputArname">Ar name :</label>
+                                        {{ $ourservice->Arname }}
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold" for="exampleInputEndescription">En Description :</label>
+                                        {{ $ourservice->Endescription }}
+                                    </div>
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold" for="exampleInputArdescription">Ar Description :</label>
+                                        {{ $ourservice->Ardescription }}
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-4">
+                                        <label class="zw_text_AF2245 poppins-bold" for="exampleInputStatus">Status :</label>
+                                        {{ $ourservice->Status }}
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+        </div>
+        <!--/.col (right) -->
+    </div>
+    </section>
+    </div>
+    @include('layouts.footer')
+    </div>
+</body>
+</html>
