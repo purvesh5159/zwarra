9abd04069d0d9e4396a94170b6e30b5388418a8e
mital <mital.k@biztechnosys.com>
2024-03-14 21:43:47 +0530

    all doctors module

---
+++ resources/views/alldoctors/show.blade.php
@@ -0,0 +1,101 @@
+@include('layouts.headermodule')
+
+<body class="hold-transition sidebar-mini zw_sidebar">
+    <div class="wrapper">
+        @include('layouts.header')
+        @include('layouts.sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="row  mb-3">
+                    <div class="col-sm-6 zw_back">
+                        <a href="{{ route('alldoctors.index') }}" class="poppins-medium zw_18 zw_text_333333 zw_a zw_back">
+                            <i class="fas fa-arrow-left right mr-2"></i>Back
+                        </a>
+                    </div>
+                    <div class="col-sm-6 ">
+                        <a class="btn zw_btn float-right m-2" href="{{ route('alldoctors.edit',$alldoctor->id) }}">Update</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- <a class="btn btn-primary m-2" href="{{ route('alldoctors.index') }}"> Back</a> -->
+                    <!-- <a class="btn btn-primary float-right m-2" href="{{ route('alldoctors.edit',$alldoctor->id) }}">Update</a> -->
+                    <!-- left column -->
+                    <div class="col-md-12">
+                        <h3 class="poppins-semibold zw_34 zw_text_AF2245 mt-4 mb-4">Show Doctors</h3>
+
+                        <!-- general form elements -->
+                        <div class="card card-primary">
+                            <!-- <div class="card-header">
+                                <h3 class="card-title">Show Doctors</h3>
+                            </div> -->
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <div class="card-body">
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputErname">En name :</label>
+                                        {{ $alldoctor->Enname }}
+                                    </div>
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputErname">Ar name :</label>
+                                        {{ $alldoctor->Arname }}
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputEmail1">Email :</label>
+                                        {{ $alldoctor->Email }}
+                                    </div>
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputPassword">Password :</label>
+                                        {{ $alldoctor->Password }}
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputLicenceno">Licenceno :</label>
+                                        {{ $alldoctor->Licenceno }}
+                                    </div>
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputphone">Phone :</label>
+                                        {{ $alldoctor->Phone }}
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputYearofexp">Year Of Experience :</label>
+                                        {{ $alldoctor->Yearofexp }}
+                                    </div>
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputPosition">Doctor Position :</label>
+                                        {{ $alldoctor->Position }}
+                                    </div>
+                                </div>
+                                <div class="form-row zw_form_row">
+                                    <div class="form-group  zw_18 poppins-medium col-md-6">
+                                        <label class="zw_text_AF2245 poppins-bold"  for="exampleInputMedicalspec">Medical Specialties :</label>
+                                        {{ $alldoctor->Medicalspec }}
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+        </div>
+        <!--/.col (right) -->
+    </div>
+    </section>
+    </div>
+    @include('layouts.footer')
+    </div>
+</body>
+
+</html>
